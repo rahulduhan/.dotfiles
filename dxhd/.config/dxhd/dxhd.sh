f2b274313217edce3bb003d2f1899c81eb5ba10a
xxdrd <rahulduhan9876@gmail.com>
2022-06-28 23:21:44 +0530

    updated keybinds

--- dxhd/.config/dxhd/dxhd.sh
+++ dxhd/.config/dxhd/dxhd.sh
@@ -2,5 +2,42 @@
 
 ##---Basic binds
 
+### Launch Application
+
+## Terminal
 #super + Return
 $TERMINAL
+
+## File Manager
+#super + f
+pcmanfm
+
+### Media Controls
+
+## Increase Volume
+#XF86AudioRaiseVolume
+volume up
+
+## Decrease Volume
+#XF86AudioLowerVolume
+volume down
+
+## Mute Volume
+#XF86AudioMute
+volume toggle
+
+## Stop Song
+#XF86AudioStop
+mpc stop
+
+## Play Song
+#XF86AudioPlay
+mpc toggle
+
+## Previous Song
+#XF86AudioPrev
+mpc prev
+
+## Next Song
+#XF86AudioNext
+mpc next
